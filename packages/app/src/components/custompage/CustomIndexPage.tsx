715d916de891e7d984aea36a9f196964d865ea43
sweco-se1g7y <daniel.bjorkman@sweco.se>
2025-09-22 13:39:10 +0200

    feature: added column selection dropdown menu

--- packages/app/src/components/custompage/CustomIndexPage.tsx
+++ packages/app/src/components/custompage/CustomIndexPage.tsx
@@ -5,17 +5,58 @@ import {
   SupportButton,
 } from '@backstage/core-components';
 import { useApi, configApiRef } from '@backstage/core-plugin-api';
-import { CatalogTable } from '@backstage/plugin-catalog';
+import {
+  CatalogTable,
+  CatalogTableColumnsFunc,
+} from '@backstage/plugin-catalog';
 import {
   EntityListProvider,
   CatalogFilterLayout,
   EntityKindPicker,
 } from '@backstage/plugin-catalog-react';
-import React from 'react';
+import React, { useState } from 'react';
 
-export const CustomIndexPage = ({kind}:{kind:string}) => {
+export const CustomIndexPage = ({ kind }: { kind: string }) => {
   const orgName =
     useApi(configApiRef).getOptionalString('organization.name') ?? 'Backstage';
+
+  const columnOptions = [
+    { key: 'Name', label: 'Name' },
+    { key: 'System', label: 'System' },
+    { key: 'Owner', label: 'Owner' },
+    { key: 'Type', label: 'Type' },
+    { key: 'Lifecycle', label: 'Lifecycle' },
+    { key: 'Description', label: 'Description' },
+    { key: 'Tags', label: 'Tags' },
+  ];
+
+  const [selectedColumns, setSelectedColumns] = useState<
+    Record<string, boolean>
+  >(
+    columnOptions.reduce((columns, option) => {
+      columns[option.key] = true;
+      return columns;
+    }, {} as Record<string, boolean>),
+  );
+
+  const handleColumnToggle = (column: string) => {
+    setSelectedColumns(prevColumns => ({
+      ...prevColumns,
+      [column]: !prevColumns[column],
+    }));
+  };
+
+  const getColumns: CatalogTableColumnsFunc = entityListContext => {
+    if (entityListContext.filters.kind?.value === kind) {
+      return CatalogTable.defaultColumnsFunc(entityListContext).filter(
+        column => selectedColumns[column.title as string],
+      );
+    }
+
+    return CatalogTable.defaultColumnsFunc(entityListContext);
+  };
+
+  const [dropdownVisible, setDropdownVisible] = useState(false);
 
   return (
     <PageWithHeader title={orgName} themeId="home">
@@ -23,15 +64,44 @@ export const CustomIndexPage = ({kind}:{kind:string}) => {
         <ContentHeader title="">
           <SupportButton>All your software catalog entities</SupportButton>
         </ContentHeader>
+
+        <div
+          className="column-menu"
+        >
+          <button onClick={() => setDropdownVisible(!dropdownVisible)}>
+            Columns
+          </button>
+          {dropdownVisible && (
+            <div className="column-menu-dropdown"
+            style={{
+                display: "grid",
+                position: "absolute",
+                backgroundColor: "#f9f9f9",
+                zIndex: 1,
+            }}>
+              {columnOptions.map(option => (
+                <label key={option.key}>
+                  <input
+                    type="checkbox"
+                    checked={selectedColumns[option.key]}
+                    onChange={() => handleColumnToggle(option.key)}
+                  />
+                  {option.label}
+                </label>
+              ))}
+            </div>
+          )}
+        </div>
+
         <EntityListProvider pagination>
           <CatalogFilterLayout>
             <div className="catalog-filter-container" hidden={true}>
               <CatalogFilterLayout.Filters>
-                <EntityKindPicker initialFilter={kind} hidden={true} />
+                <EntityKindPicker initialFilter={kind} />
               </CatalogFilterLayout.Filters>
             </div>
             <CatalogFilterLayout.Content>
-              <CatalogTable />
+              <CatalogTable columns={getColumns} />
             </CatalogFilterLayout.Content>
           </CatalogFilterLayout>
         </EntityListProvider>
