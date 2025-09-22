7d66508c8ee416b1280bea7eb4cff5ec70b1c7dd
sweco-se1g7y <daniel.bjorkman@sweco.se>
2025-09-22 14:22:29 +0200

    feature: broke out CSS styling

---
+++ packages/app/src/theme/geoportiaopen.ts
@@ -0,0 +1,38 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useGeoportiaOpenStyles = makeStyles(theme => ({
+  catalogFilterContainer: {
+  },
+  columnMenu: {
+    maxWidth: '83.333333%',
+    minHeight: 32,
+    position: 'relative',
+    zIndex: 1,
+    marginBottom: 5,
+  },
+  columnMenuDropdownContainer: {
+    position: 'absolute',
+    minWidth: 150,
+    right: 0,
+    top: 0,
+    backgroundColor: theme.palette.background.default,
+    color: theme.palette.text.primary,
+  },
+  columnMenuButton: {
+    position: 'relative',
+    width: '100%',
+    padding: 5,
+    borderRadius: 5,
+  },
+  columnMenuDropdown: {
+    display: 'grid',
+    border: `1px solid ${theme.palette.divider}`,
+    position: 'relative',
+    padding: 5,
+    borderRadius: 5,
+  },
+  columnMenuLabel: {
+  },
+  columnMenuCheckbox: {
+  }
+}));
