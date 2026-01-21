536b1d778dd13ea4dfaccc399fdc5dabd18f6154
sweco-se1g7y <daniel.bjorkman@sweco.se>
2026-01-21 15:25:31 +0100

    Revert "Merge branch 'dev' into feature/add-table-filter"
    This reverts commit fcb63df1e9acffe222f395f2fe66f2b340255c17, reversing
    changes made to e2aa296065b3ae732005fb7d8fcd271d7e5ab4b2.

--- packages/app/src/theme/geoportiaopen.ts
+++ packages/app/src/theme/geoportiaopen.ts
@@ -2,19 +2,26 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useGeoportiaOpenStyles = makeStyles(theme => ({
   catalogFilterContainer: {
+    maxWidth: '83.333333%',
+    minHeight: 32,
+    position: 'relative',
+    zIndex: 1,
+    marginBottom: 15,
+  },
+  catalogMenuContainer: {
+    position: 'absolute',
+    right: 0,
+    top: 0,
+    display: 'flex',
   },
   columnMenu: {
-    maxWidth: '83.333333%',
     minHeight: 32,
     position: 'relative',
     zIndex: 1,
     marginBottom: 5,
   },
   columnMenuDropdownContainer: {
-    position: 'absolute',
     minWidth: 150,
-    right: 0,
-    top: 0,
     backgroundColor: theme.palette.background.default,
     color: theme.palette.text.primary,
   },
@@ -27,5 +34,42 @@ export const useGeoportiaOpenStyles = makeStyles(theme => ({
   },
   columnMenuDropdown: {
     padding: 5,
+  },
+  filterMenu: {
+    minHeight: 32,
+    position: 'relative',
+    zIndex: 1,
+    marginBottom: 5,
+    marginRight: 10,
+  },
+  filterMenuDropdownContainer: {
+    minWidth: 150,
+    backgroundColor: theme.palette.background.default,
+    color: theme.palette.text.primary,
+  },
+  filterMenuButton: {
+    position: 'relative',
+    width: '100%',
+    padding: 5,
+    borderRadius: 5,
+    border: `1px solid ${theme.palette.divider}`,
+  },
+  filterMenuDropdown: {
+    padding: 5,
+  },
+  filterMenuDropdownForm: {
+    padding: 5,
+    display: 'inline-grid',
+  },
+  filterMenuDropdownLabel: {
+    padding: 5,
+  },
+  flexBoxColumn: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  dropdownMenu: {
+    minWidth: 150,
+    padding: '0 5px',
   }
 }));
