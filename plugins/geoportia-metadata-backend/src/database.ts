41ebf4f5d87e0ede27401372f88c60cd4fb21a75
sweco-se1c8x <ayham.darwish@sweco.se>
2026-03-13 15:34:24 +0100

    Adjust the metadata storage schema

--- plugins/geoportia-metadata-backend/src/database.ts
+++ plugins/geoportia-metadata-backend/src/database.ts
@@ -1,9 +1,12 @@
 export interface TableRow {
   id: number;
+  entity_ref: string;
   database: string;
   name: string;
   version: number;
   active: boolean;
+  schema: unknown;
+  metadata: unknown;
   title: string;
   owner: string;
   properties: string;
