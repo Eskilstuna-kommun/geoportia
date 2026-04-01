990125f2ce35c1aeca5e2aee6240fdd5900a4ef7
sweco-se1c8x <ayham.darwish@sweco.se>
2026-04-01 13:56:24 +0200

    remove old code

--- plugins/geoportia-metadata-backend/migrations/20250124153600_init.js
+++ plugins/geoportia-metadata-backend/migrations/20250124153600_init.js
@@ -4,7 +4,7 @@
  * @param {import('knex').Knex} knex
  */
 exports.up = async function up(knex) {
-  await knex.schema.createTable('table', table => {
+  await knex.schema.createTable('geoportia_metadata', table => {
     table.increments('id');
 
     table.string('entity_ref').notNullable();
@@ -12,26 +12,11 @@ exports.up = async function up(knex) {
     table.jsonb('schema').notNullable();
     table.jsonb('metadata').notNullable();
   });
-  await knex.schema.createTable('attribute', table => {
-    table
-      .integer('table_id')
-      .unsigned()
-      .notNullable()
-      .references('id')
-      .inTable('table');
-    table.string('name').notNullable();
-    table.string('title').notNullable();
-    table.string('type').notNullable();
-    table.jsonb('properties').notNullable();
-
-    table.primary(['table_id', 'name']);
-  });
 };
 
 /**
  * @param {import('knex').Knex} knex
  */
 exports.down = async function down(knex) {
-  await knex.schema.dropTable('attribute');
-  await knex.schema.dropTable('table');
+  await knex.schema.dropTable('geoportia_metadata');
 };
--- plugins/geoportia-metadata-backend/src/database.ts
+++ plugins/geoportia-metadata-backend/src/database.ts
@@ -1,20 +1,6 @@
 export interface TableRow {
   id: number;
   entity_ref: string;
-  database: string;
-  name: string;
-  version: number;
-  active: boolean;
   schema: unknown;
   metadata: unknown;
-  title: string;
-  owner: string;
-  properties: string;
-}
-export interface AttributeRow {
-  table_id: number;
-  name: string;
-  title: string;
-  type: string;
-  properties: string;
 }
