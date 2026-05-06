72d9c9e6eabbdb827c17ad73e2b5faa44a46e94b
sweco-se1c8x <ayham.darwish@sweco.se>
2026-05-06 16:36:20 +0200

    add migration metadata suggestion

---
+++ plugins/geoportia-metadata-backend/migrations/20260506100000_metadata_suggestions.js
@@ -0,0 +1,31 @@
+// @ts-check
+
+/**
+ * @param {import('knex').Knex} knex
+ */
+exports.up = async function up(knex) {
+  await knex.schema.createTable('geoportia_metadata_suggestions', table => {
+    table.increments('id');
+
+    table
+      .integer('metadata_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('geoportia_metadata')
+      .onDelete('CASCADE');
+
+    table.jsonb('metadata').notNullable();
+
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+
+    table.string('suggested_by').notNullable();
+  });
+};
+
+/**
+ * @param {import('knex').Knex} knex
+ */
+exports.down = async function down(knex) {
+  await knex.schema.dropTable('geoportia_metadata_suggestions');
+};
